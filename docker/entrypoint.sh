e900511ed7074c5c4edc92faa27acf2081a56037
AlanShoebill <alan_rafael_1997@hotmail.com>
2023-08-29 18:11:50 -0600

    esto no es zaki

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py collectstatic --noinput
 # Run server
 # python manage.py runserver 0.0.0.0:8000
-gunicorn --bind 0.0.0.0:8000 -t 2400 zaki.wsgi --log-level debug
+gunicorn --bind 0.0.0.0:8000 -t 2400 prueback.wsgi --log-level debug
